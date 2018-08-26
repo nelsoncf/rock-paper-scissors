fc860e8f39c6a7ed923465cef214d39a74ccda95
Nelson Campos <ncamposfh@gmail.com>
2018-08-26 20:04:15 -0300

    onClick nas imagens

--- app/src/main/java/com/nelsoncf/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/nelsoncf/rockpaperscissors/MainActivity.java
@@ -2,6 +2,7 @@ package com.nelsoncf.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +11,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void selecionarPedra(View view) {
+        this.opcaoSelecionada("pedra");
+    }
+
+    public void selecionarPapel(View view) {
+        this.opcaoSelecionada("papel");
+    }
+
+    public void selecionarTesoura(View view) {
+        this.opcaoSelecionada("tesoura");
+    }
+
+
+    public void opcaoSelecionada(String escolhaUsuario){
+
+    }
+
+
 }
