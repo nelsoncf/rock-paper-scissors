93618af6a6a95212b5559358d24b1df1d53c9868
Nelson Campos <ncamposfh@gmail.com>
2018-08-26 20:53:47 -0300

    fim poc lembrando como funciona esses apps depois de 5 anos

--- app/src/main/java/com/nelsoncf/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/nelsoncf/rockpaperscissors/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.Random;
 
@@ -31,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
     public void opcaoSelecionada(String escolhaUsuario){
 
         ImageView imagemResultado = (ImageView) findViewById(R.id.imageResultado);
+        TextView textoResultado = (TextView) findViewById(R.id.textResultado);
 
         String[] opcoes = { "pedra",  "papel", "tesoura"};
         int numero = new Random().nextInt(3);
@@ -47,6 +49,14 @@ public class MainActivity extends AppCompatActivity {
                 imagemResultado.setImageResource(R.drawable.tesoura);
                 break;
 
+        }
+
+        if((escolhaApp =="pedra" && escolhaUsuario == "tesoura") || (escolhaApp == "papel" && escolhaUsuario == "pedra") || (escolhaApp == "tesoura" && escolhaUsuario == "papel")){
+            textoResultado.setText("Você Perdeu");
+        } else if ((escolhaUsuario =="pedra" && escolhaApp == "tesoura") || (escolhaUsuario == "papel" && escolhaApp == "pedra") || (escolhaUsuario == "tesoura" && escolhaApp == "papel")){
+            textoResultado.setText("Você Ganhou");
+        } else {
+            textoResultado.setText("Empate");
         }
 
 
